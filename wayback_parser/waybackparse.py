2623fc842a0c5ab0418f6364d179f5035df48e5e
Jeya Seelan <45196752+jeyaseelan8@users.noreply.github.com>
2020-12-12 16:37:37 +0530

    commit

--- wayback_parser/waybackparse.py
+++ wayback_parser/waybackparse.py
@@ -16,7 +16,7 @@ for i in range(1,len(rjson)):
 #print(*urls,sep='\n')
 
 
-extensions=[".asp",".aspx",".cer",".js",".cfm",".cfml",".rb",".php",".php3",".php4",".php5",".jsp",".json",".apk",".ods",".xls",".xlsm",".xlsx",".bak",".cab",".cpl",".dmp",".drv",".tmp",".sys",".doc",".docx",".pdf",".txt",".wpd",".bat",".bin",".cgi",".pl",".py",".exe",".gadget",".jar",".msi",".wsf",".csv",".dat",".db",".dbf",".log",".mdb",".sav",".sql",".tar",".xml",".7z",".arj",".deb",".pkg",".rar",".rpm",".tar.gz",".z",".zip",".bin",".dmg",".iso",".toast",".vcd",".email",".eml",".emlx",".msg",".oft",".ost",".pst",".vcf",".shtm",".shtml",".phtm",".phtml",".jhtml",".conf",".yml",".config",".yaml",".wsdl",".java",".key",".html",".sh"]
+extensions=[".asp",".do",".aspx",".cer",".js",".cfm",".cfml",".rb",".php",".php3",".php4",".php5",".jsp",".json",".apk",".ods",".xls",".xlsm",".xlsx",".bak",".cab",".cpl",".dmp",".drv",".tmp",".sys",".doc",".docx",".pdf",".txt",".wpd",".bat",".bin",".cgi",".pl",".py",".exe",".gadget",".jar",".msi",".wsf",".csv",".dat",".db",".dbf",".log",".mdb",".sav",".sql",".tar",".xml",".7z",".arj",".deb",".pkg",".rar",".rpm",".tar.gz",".z",".zip",".bin",".dmg",".iso",".toast",".vcd",".email",".eml",".emlx",".msg",".oft",".ost",".pst",".vcf",".shtm",".shtml",".phtm",".phtml",".jhtml",".conf",".yml",".config",".yaml",".wsdl",".java",".key",".html",".sh"]
 withext=[]
 for i in range(len(urls)):
     u=urls[i]
