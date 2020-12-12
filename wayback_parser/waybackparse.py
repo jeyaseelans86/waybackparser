8758f40e6c4bf762f8f978c658270107677d6645
Jeya Seelan <45196752+jeyaseelan8@users.noreply.github.com>
2020-12-12 16:10:49 +0530

    m

--- wayback_parser/waybackparse.py
+++ wayback_parser/waybackparse.py
@@ -2,8 +2,6 @@ import sys
 import requests
 from pathlib import Path
 
-
-
 u=sys.argv[1]
 
 
@@ -15,7 +13,7 @@ for i in range(1,len(rjson)):
     url=rjson[i][2]
     urls.append(url)
     sorted(set(urls))
-#print(*urls,sep='\n')
+print(*urls,sep='\n')
 
 
 extensions=[".asp",".aspx",".cer",".js",".cfm",".cfml",".rb",".php",".php3",".php4",".php5",".jsp",".json",".apk",".ods",".xls",".xlsm",".xlsx",".bak",".cab",".cpl",".dmp",".drv",".tmp",".sys",".doc",".docx",".pdf",".txt",".wpd",".bat",".bin",".cgi",".pl",".py",".exe",".gadget",".jar",".msi",".wsf",".csv",".dat",".db",".dbf",".log",".mdb",".sav",".sql",".tar",".xml",".7z",".arj",".deb",".pkg",".rar",".rpm",".tar.gz",".z",".zip",".bin",".dmg",".iso",".toast",".vcd",".email",".eml",".emlx",".msg",".oft",".ost",".pst",".vcf",".shtm",".shtml",".phtm",".phtml",".jhtml",".conf",".yml",".config",".yaml",".wsdl",".java",".key",".html",".sh"]
